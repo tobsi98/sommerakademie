f51a471dc316d5eed5fb9ec8b7c209bb5c43b6c4
Franziska <e12208158@student.tuwien.ac.at>
2023-07-07 16:58:48 +0200

    add location param to all contraints

--- optimization.py
+++ optimization.py
@@ -24,9 +24,9 @@ def opt_dc(nr_time_steps, nr_cooling_machines=4, nr_fwp=2, nr_locations=3, cop=4
     model.km_min_pow = en.Param(model.KM, mutable=True)
     model.km_max_pow = en.Param(model.KM, mutable=True)
     # load in [kW]
-    model.cooling_load = en.Param(model.T, mutable=True)
+    model.cooling_load = en.Param(model.LOCA, model.T, mutable=True)
     # Water flow in [m3/h]
-    model.water_flow = en.Param(model.T, mutable=True)
+    model.water_flow = en.Param(model.LOCA, model.T, mutable=True)
     model.cop = en.Param(initialize=cop)
     # incentive in [EUR/MWh]
     model.incentive = en.Param(mutable=True)
@@ -35,64 +35,64 @@ def opt_dc(nr_time_steps, nr_cooling_machines=4, nr_fwp=2, nr_locations=3, cop=4
     model.maintenance_costs = en.Param(mutable=True)
 
     # ############################## Variables ################################
-    model.km_status = en.Var(model.KM, model.T, domain=en.Binary)
-    model.km_cwp_status = en.Var(model.KM, model.KM_CWP, model.T, domain=en.Binary)
+    model.km_status = en.Var(model.LOCA, model.KM, model.T, domain=en.Binary)
+    model.km_cwp_status = en.Var(model.LOCA, model.KM, model.KM_CWP, model.T, domain=en.Binary)
     # cold water pump power [kW]
-    model.cwpp = en.Var(model.T, domain=en.NonNegativeReals)
-    model.km_generation_power = en.Var(model.KM, model.T, domain=en.NonNegativeReals)
-    model.elec_load = en.Var(model.T, domain=en.NonNegativeReals)
-    model.fwp_power =en.Var(model.FWP,model.T,domain=en.NonNegativeReals)
+    model.cwpp = en.Var(model.LOCA, model.T, domain=en.NonNegativeReals)
+    model.km_generation_power = en.Var(model.LOCA, model.KM, model.T, domain=en.NonNegativeReals)
+    model.elec_load = en.Var(model.LOCA, model.T, domain=en.NonNegativeReals)
+    model.fwp_power =en.Var(model.LOCA, model.FWP,model.T,domain=en.NonNegativeReals)
 
     # ############################## Constraints ##############################
     
-    def cover_load_rule(model, t):
-        return sum(model.km_generation_power[i, t] for i in model.KM) >= model.cooling_load[t]
-    model.cover_load = en.Constraint(model.T, rule=cover_load_rule)
+    def cover_load_rule(model, loca, t):
+        return sum(model.km_generation_power[loca, i, t] for i in model.KM) >= model.cooling_load[loca, t]
+    model.cover_load = en.Constraint(model.LOCA, model.T, rule=cover_load_rule)
 
-    def km_min_generation_rule(model, i, t): #km = kältemaschine 
-        return model.km_status[i, t] * model.km_min_pow[i] <= model.km_generation_power[i, t]
-    model.km_min_generation = en.Constraint(model.KM, model.T, rule=km_min_generation_rule)
+    def km_min_generation_rule(model, loca, i, t): #km = kältemaschine 
+        return model.km_status[loca, i, t] * model.km_min_pow[i] <= model.km_generation_power[loca, i, t]
+    model.km_min_generation = en.Constraint(model.LOCA, model.KM, model.T, rule=km_min_generation_rule)
 
-    def km_max_generation_rule(model, i, t):
-        return model.km_status[i, t] * model.km_max_pow[i] >= model.km_generation_power[i, t]
-    model.km_max_generation = en.Constraint(model.KM, model.T, rule=km_max_generation_rule)
+    def km_max_generation_rule(model, loca, i, t):
+        return model.km_status[loca, i, t] * model.km_max_pow[i] >= model.km_generation_power[loca, i, t]
+    model.km_max_generation = en.Constraint(model.LOCA, model.KM, model.T, rule=km_max_generation_rule)
 
 
-    def cwp_status_rule(model, i, j, t): #cwp = cool water pump
-        return model.km_cwp_status[i, j, t] <= model.km_status[i, t]
-    model.cwp_status = en.Constraint(model.KM, model.KM_CWP, model.T, rule=cwp_status_rule)
+    def cwp_status_rule(model, loca, i, j, t): #cwp = cool water pump
+        return model.km_cwp_status[loca, i, j, t] <= model.km_status[loca, i, t]
+    model.cwp_status = en.Constraint(model.LOCA, model.KM, model.KM_CWP, model.T, rule=cwp_status_rule)
 
-    def cwp_wf_rule(model, t):
-        return model.water_flow[t] <= sum(model.km_cwp_status[i, 'Small', t] * 300 + model.km_cwp_status[i, 'Large', t] * 420 for i in model.KM)
-    model.cwp_wf = en.Constraint(model.T, rule=cwp_wf_rule)
+    def cwp_wf_rule(model, loca, t):
+        return model.water_flow[loca, t] <= sum(model.km_cwp_status[loca, i, 'Small', t] * 300 + model.km_cwp_status[loca, i, 'Large', t] * 420 for i in model.KM)
+    model.cwp_wf = en.Constraint(model.LOCA, model.T, rule=cwp_wf_rule)
 
-    def cwp_power_rule(model, t):
-        return model.cwpp[t] == sum(model.km_cwp_status[i, 'Small', t] * 23 + model.km_cwp_status[i, 'Large', t] * 40.1 for i in model.KM)
-    model.cwp_power = en.Constraint(model.T, rule=cwp_power_rule)
+    def cwp_power_rule(model, loca, t):
+        return model.cwpp[loca, t] == sum(model.km_cwp_status[loca, i, 'Small', t] * 23 + model.km_cwp_status[loca, i, 'Large', t] * 40.1 for i in model.KM)
+    model.cwp_power = en.Constraint(model.LOCA, model.T, rule=cwp_power_rule)
     
-    def fwp1_power_rule(model, t):
-      s1= model.km_status[0,t]
-      s2= model.km_status[1,t]
-      return model.fwp_power[0,t] >= 18*s1+18*s2+(s1+s2-1)*6.95*s1+(s1+s2-1)*6.95*s2
-    model.fwp1_power = en.Constraint(model.T, rule=fwp1_power_rule)
+    def fwp1_power_rule(model, loca, t):
+      s1= model.km_status[loca, 0,t]
+      s2= model.km_status[loca, 1,t]
+      return model.fwp_power[loca, 0,t] >= 18*s1+18*s2+(s1+s2-1)*6.95*s1+(s1+s2-1)*6.95*s2
+    model.fwp1_power = en.Constraint(model.LOCA, model.T, rule=fwp1_power_rule)
     
-    def fwp2_power_rule(model, t):
-      s1= model.km_status[2,t]
-      s2= model.km_status[3,t]
-      return model.fwp_power[1,t] >= 18*s1+18*s2+(s1+s2-1)*6.95*s1+(s1+s2-1)*6.95*s2
-    model.fwp2_power = en.Constraint(model.T, rule=fwp2_power_rule)
+    def fwp2_power_rule(model, loca, t):
+      s1= model.km_status[loca, 2,t]
+      s2= model.km_status[loca, 3,t]
+      return model.fwp_power[loca, 1,t] >= 18*s1+18*s2+(s1+s2-1)*6.95*s1+(s1+s2-1)*6.95*s2
+    model.fwp2_power = en.Constraint(model.LOCA, model.T, rule=fwp2_power_rule)
     
 
-    def electricity_load_rule(model, t):
-        return model.elec_load[t] >= sum(model.km_generation_power[i, t] / model.cop for i in model.KM)
-    model.electricity_load = en.Constraint(model.T, rule=electricity_load_rule)
+    def electricity_load_rule(model, loca, t):
+        return model.elec_load[loca, t] >= sum(model.km_generation_power[loca, i, t] / model.cop for i in model.KM)
+    model.electricity_load = en.Constraint(model.LOCA, model.T, rule=electricity_load_rule)
 
     def obj_rule(model):
         # OBJECTIVE FUNC: Minimize Elec Costs
-        return sum(((model.cwpp[t] + model.fwp_power[0,t] + model.fwp_power[1,t]) * model.electricity_price[t] + 
-                   model.elec_load[t] * (model.electricity_price[t] + 
+        return sum(sum(((model.cwpp[loca, t] + model.fwp_power[loca, 0,t] + model.fwp_power[loca, 1,t]) * model.electricity_price[t] + 
+                   model.elec_load[loca, t] * (model.electricity_price[t] + 
                model.maintenance_costs - model.incentive)) / LOAD_STEPS_PER_HOUR
-                for t in model.T)
+                for t in model.T)for loca in model.LOCA)
     model.obj = en.Objective(rule=obj_rule, sense=en.minimize)
     return model.create_instance()
 
@@ -112,9 +112,13 @@ if __name__ == "__main__":
     df = pd.read_csv(f_load)
     data_location0 = df[(df['utility']==0)]
     data_location1 = df[(df['utility']==1)]
+    data_location1.reset_index(inplace = True, drop=True)
     data_location2 = df[(df['utility']==2)]
-    
-    df = data_location2
+    data_location2.reset_index(inplace = True, drop=True)
+    data_location1.rename(columns={'load': 'load1'}, inplace=True)
+    data_location2.rename(columns={'load': 'load2'}, inplace=True)
+    data = pd.concat([data_location0['t'], data_location0['load'], data_location1['load1'], data_location2['load2']], axis=1)
+    #df = data_location2
 
     
     df.plot(y=['load', 'water_flow'])
